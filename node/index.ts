b01cb2e588d8384b414805f4f75b7010214da5fc
Tiago de Andrade Freire <tiago.freire@cubos.io>
2022-09-19 18:28:01 -0300

    feat: affiliates and WIP node service for styles

--- node/index.ts
+++ node/index.ts
@@ -1,8 +1,11 @@
-import { ClientsConfig, LRUCache, method, Service, ServiceContext, UserInputError } from "@vtex/api";
-import { Clients } from "./clients";
+import type { ClientsConfig, ServiceContext } from '@vtex/api'
+import { LRUCache, method, Service, UserInputError } from '@vtex/api'
+import type { Colors } from 'vtex.styles-graphql'
 
-const TIMEOUT_MS = 800;
-const memoryCache = new LRUCache<string, never>({ max: 5000 });
+import { Clients } from './clients'
+
+const TIMEOUT_MS = 800
+const memoryCache = new LRUCache<string, never>({ max: 5000 })
 
 const clients: ClientsConfig<Clients> = {
   implementation: Clients,
@@ -15,37 +18,82 @@ const clients: ClientsConfig<Clients> = {
       memoryCache,
     },
   },
-};
+}
 
+// type PossibleColors =
+//   | BackgroundColors
+//   | HoverBackgroundColors
+//   | ActiveBackgroundColors
+//   | TextColors
+//   | VisitedTextColors
+//   | HoverTextColors
+//   | ActiveTextColors
+//   | BorderColors
+//   | HoverBorderColors
+//   | ActiveBorderColors
+//   | OnColors
+//   | HoverOnColors
+//   | ActiveOnColors
 export default new Service({
   clients,
   routes: {
     collection: method({
       GET: async (context: ServiceContext<Clients>) => {
-        // context.clients.search.products({
-        //   collection
-        // })
-
         const {
           vtex: {
             route: {
               params: { collection },
             },
           },
-        } = context;
-
-        console.log(collection)
+        } = context
 
         if (!collection) {
-          throw new UserInputError("collection is required");
+          throw new UserInputError('collection is required')
         }
 
-        const response = await context.clients.collection.getCollection(collection as string);
+        const response = await context.clients.collection.getCollection(
+          collection as string
+        )
 
-        context.body = response;
-        context.status = 200;
-        context.set("Cache-Control", "no-cache");
+        context.body = response
+
+        context.status = 200
+        context.set('Cache-Control', 'no-cache')
+      },
+    }),
+    teste: method({
+      GET: async (context: ServiceContext<Clients>) => {
+        const {
+          clients: { stylesGraphql },
+        } = context
+
+        const styles = (await stylesGraphql.getStyles()) ?? {}
+
+        const cssVariables = Object.keys(styles)
+          .map(
+            majorKey =>
+              // styles[majorKey as keyof Colors]?.map(
+              //   minorKey =>
+              //     `--${majorKey}-${minorKey}: ${
+              //       styles[majorKey as keyof Colors]
+              //         ? styles[majorKey as keyof Colors]
+              //           ? [minorKey as keyof PossibleColors]
+              //           : ''
+              //         : ''
+              //     };`
+              // )
+
+              `--${majorKey}-: ${JSON.stringify(
+                styles[majorKey as keyof Colors]
+              )}`
+          )
+          .join('\n')
+
+        context.body = cssVariables
+        context.status = 200
+        context.set('Content-Type', 'application/json')
+        context.set('Cache-Control', 'no-cache')
       },
     }),
   },
-});
+})
